6419968e4bb746ba3b2f70a9093bd0efd28ce3d1
Gio d'Amelio <giodamelio@gmail.com>
2022-04-26 22:14:50 -0500

    Skip the timer ui and show a message confirming the timer being set

--- app/src/main/java/com/giodamelio/quicktimer/MainActivity.kt
+++ app/src/main/java/com/giodamelio/quicktimer/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.provider.AlarmClock
 import android.util.Log
 import android.view.View
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +19,16 @@ class MainActivity : AppCompatActivity() {
         val seconds = view.tag.toString().toInt()
         Log.d("MainActivity", "Setting Timer for $seconds seconds")
 
+        // Start the timer
         val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
             putExtra(AlarmClock.EXTRA_MESSAGE, "QuickTimer")
             putExtra(AlarmClock.EXTRA_LENGTH, seconds)
-            //putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+            putExtra(AlarmClock.EXTRA_SKIP_UI, true)
         }
         startActivity(intent)
+
+        // Let the user know
+        val notificationSnackBar = Snackbar.make(view, "Timer set for ${seconds / 60} minutes", LENGTH_SHORT)
+        notificationSnackBar.show()
     }
 }
