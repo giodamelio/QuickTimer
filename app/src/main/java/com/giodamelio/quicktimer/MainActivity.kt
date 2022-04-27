d189723163c21d47715b9f77c3a406dbfc3023d9
Gio d'Amelio <giodamelio@gmail.com>
2022-04-26 22:05:34 -0500

    Add a few buttons and trigger the timer intent based on their tag

--- app/src/main/java/com/giodamelio/quicktimer/MainActivity.kt
+++ app/src/main/java/com/giodamelio/quicktimer/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.giodamelio.quicktimer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun setTimer(view: View) {
+        val seconds = view.tag.toString().toInt()
+        Log.d("MainActivity", "Setting Timer for $seconds seconds")
+
+        val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, "QuickTimer")
+            putExtra(AlarmClock.EXTRA_LENGTH, seconds)
+            //putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+        }
+        startActivity(intent)
     }
 }
